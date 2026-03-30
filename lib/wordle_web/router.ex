613dc1141a9f908a198d2c64e93154de2d4922e4
Minvydas <minderrx2@gmail.com>
2026-03-30 19:30:01 +0100

    change route away from default

--- lib/wordle_web/router.ex
+++ lib/wordle_web/router.ex
@@ -17,7 +17,7 @@ defmodule WordleWeb.Router do
   scope "/", WordleWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", MainPage, :home
   end
 
   # Other scopes may use custom stacks.
